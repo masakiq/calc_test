d0f029d23d6cfa5f5b4b23ca1979e469102b3e99
maeda1150 <masaki19770108@gmail.com>
2018-07-15 19:05:04 +0900

    do not copy if dir is same

--- ext/calc_test/extconf.rb
+++ ext/calc_test/extconf.rb
@@ -1,4 +1,6 @@
 # this is a cheat
 require 'fileutils'
-puts "Copy all file #{__dir__}/* to #{Dir.pwd}"
-FileUtils.cp_r("#{__dir__}/.", "#{Dir.pwd}")
+unless Dir.pwd.include?(__dir__)
+  puts "Copy all file #{__dir__}/* to #{Dir.pwd}"
+  FileUtils.cp_r("#{__dir__}/.", "#{Dir.pwd}")
+end
