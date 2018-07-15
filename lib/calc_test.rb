097806c1c76cfac1585db909411684cb38f4a819
maeda1150 <masaki19770108@gmail.com>
2018-07-15 19:22:20 +0900

    fix file path

--- lib/calc_test.rb
+++ lib/calc_test.rb
@@ -3,7 +3,14 @@ require 'ffi'
 
 module CalcTest
   extend FFI::Library
-  ffi_lib File.expand_path("calc_test/calc_test.bundle", File.dirname(__FILE__))
+
+  if File.exist?('calc_test/calc_test.bundle')
+    ffi_lib File.expand_path('calc_test/calc_test.bundle', File.dirname(__FILE__))
+  else
+    ffi_lib File.expand_path('../ext/calc_test/calc_test.bundle', File.dirname(__FILE__))
+  end
+
   attach_function :add, [:int, :int], :int
   attach_function :sub, [:int, :int], :int
 end
+
